86b2ce2f71022860ed305d34197ce038e52fb018
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-02-24 00:37:29 -0500

    add login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import LoginPage from './login/login.page';
+
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/login' component={LoginPage}/>
+          { /* redirect if no route matches */ }
+          <Redirect to='/login'/>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
