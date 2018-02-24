a5c28e3c38f1ba568efd691a13720a014887f222
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-02-24 14:53:52 -0500

    add redirect from /login to /servers when logged in

--- client/src/login/login.page.js
+++ client/src/login/login.page.js
@@ -1,9 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import DiscordLoginBtn from "./discord-login-btn";
+import NixApiService from "../nix-api-service";
 
 class LoginPage extends Component {
   render() {
+    if (NixApiService.userIsLoggedIn) {
+      return (<Redirect to={`/servers`}/>);
+    }
+
     return (
       <div className="page login">
         <DiscordLoginBtn/>
