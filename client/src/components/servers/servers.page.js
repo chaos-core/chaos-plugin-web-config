94e27af1d88f346f235f54deceffc58b02aaf5f0
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-03-15 21:57:21 -0400

    move user-info component into the layout

--- client/src/components/servers/servers.page.js
+++ client/src/components/servers/servers.page.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
-import UserInfo from "../shared/user-info";
-
 const ServersPage = () => (
   <div className="page servers">
-    <UserInfo/>
     Server list!
   </div>
 );
--- client/src/layout/logged-in.layout.js
+++ client/src/layout/logged-in.layout.js
@@ -4,9 +4,11 @@ import { Route, Redirect } from 'react-router';
 import ConnectedSwitch from "../components/shared/connected-switch";
 
 import ServersPage from '../components/servers/servers.page';
+import UserInfo from '../components/shared/user-info';
 
 const LoggedInLayout = () => (
   <div>
+    <UserInfo/>
     <ConnectedSwitch>
       <Route exact path='/servers' component={ServersPage}/>
       {/* redirect if no route matches */}
