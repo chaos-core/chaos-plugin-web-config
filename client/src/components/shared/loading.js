c27ab8f066d3254bf7b68870c68f1d7d4d2eeb10
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-03-16 22:26:54 -0400

    Create loading spinner

---
+++ client/src/components/shared/loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "./loading.scss";
+
+const Loading = () => (
+  <div className={"loader"}>
+    <div className={"bounce1"} />
+    <div className={"bounce2"} />
+    <div className={"bounce3"} />
+    <div className={"bounce4"} />
+    <div className={"bounce5"} />
+  </div>
+);
+
+export default Loading;
