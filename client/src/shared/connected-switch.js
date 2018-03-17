f069ee23ef1f13620203e18bd033edcd05524d93
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-03-16 22:29:35 -0400

    remove dup components

--- client/src/shared/connected-switch.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import { Switch } from 'react-router';
-
-const ConnectedSwitch = connect(state => ({
-  location: state.router.location
-}))(Switch);
-
-export default ConnectedSwitch;
