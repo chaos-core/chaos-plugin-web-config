b53d223e7a380637b520570eda1530a743dd0f3d
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-03-17 18:41:21 -0400

    add FETCH_SERVER action

--- client/src/actions/servers.actions.js
+++ client/src/actions/servers.actions.js
@@ -1,8 +1,17 @@
+import NixApiClient from '../lib/nix-api-client';
+
 const SET_SERVER = (server) => ({
   type: 'SERVERS.SET_SERVER',
   payload: { server },
 });
 
+const FETCH_SERVER = (serverId) => (dispatch) => {
+  NixApiClient
+    .fetchServer(serverId)
+    .then((server) => dispatch(SET_SERVER(server)));
+};
+
 export {
   SET_SERVER,
+  FETCH_SERVER,
 }
