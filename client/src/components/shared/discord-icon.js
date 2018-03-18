252cb2b1dee38f9b10c2e785e77bdfad7062eeb4
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-03-17 23:24:12 -0400

    create discord-icon component

---
+++ client/src/components/shared/discord-icon.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './discord-icon.scss';
+
+const DiscordIcon = (props) => (
+  <img className={`discord-icon discord-icon-${props.type} ${props.className}`} alt={props.alt} src={getUrl(props)}/>
+);
+
+function getUrl(options) {
+  let base = 'https://cdn.discordapp.com/';
+
+  switch (options.type) {
+    case "emoji":
+      return base + `emojis/${options.iconId}.png`;
+    case "guild":
+      return base + `icons/${options.guildId}/${options.iconId}.png`;
+    case "user":
+      return base + `avatars/${options.userId}/${options.iconId}.png`;
+    case "app":
+      return base + `app-icons/${options.appId}/${options.iconId}.png`;
+    default:
+      return undefined;
+  }
+}
+
+export default DiscordIcon;
