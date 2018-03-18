07611332d2d7a4036975ab3a3f5e2433f5791c7e
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-03-18 14:11:36 -0400

    add module-list component

---
+++ client/src/components/modules/module-list.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+
+import NixApiClient from '../../lib/nix-api-client';
+
+import Loading from '../shared/loading';
+
+const mapStateToProps = (state, ownProps) => ({
+  server: state.server,
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({});
+
+class ModuleListView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetching: true,
+      modules: [],
+    }
+  }
+
+  render() {
+    return (
+      <div className={"module-list"}>
+        {
+          this.state.fetching
+            ? <Loading/>
+            : this.renderList()
+        }
+      </div>
+    );
+  }
+
+  renderList() {
+    return this.state
+      .modules
+      .map((module, index) => (
+        <div key={index}>{module.name} {module.enabled ? "✓" : "✘"}</div>
+      ));
+  }
+
+  componentDidMount() {
+    this.fetchModules();
+  }
+
+  fetchModules() {
+    NixApiClient
+      .fetchModules(this.props.server.id)
+      .then((modules) => this.setState({
+        fetching: false,
+        modules,
+      }))
+  }
+}
+
+const ModuleList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ModuleListView);
+
+export default ModuleList;
