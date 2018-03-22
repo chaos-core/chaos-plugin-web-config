5c4ca6e715480468a82fc737ceeb6e40e8e83202
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-03-21 20:05:20 -0400

    create userIsAdmin route filter

--- server/config/filters.js
+++ server/config/filters.js
@@ -1,3 +1,7 @@
+const Rx = require('rx');
+
+const Server = require('../models/server');
+
 module.exports = {
   authorize(req, res, next) {
     let JwtManager = req.app.locals.services.JwtManager;
@@ -22,5 +26,46 @@ module.exports = {
             return next(error);
         }
       });
+  },
+
+  userIsAdmin(req, res, next) {
+    let nix = req.app.locals.nix;
+    let userId = res.locals.userId;
+    let guildId = req.params.guildId;
+
+    return Rx.Observable
+      .of(nix.discord.guilds.get(guildId))
+      .flatMap((guild) =>
+        Rx.Observable.if(
+          () => guild,
+          Rx.Observable.of(guild).map((guild) => new Server(nix, guild)),
+          Rx.Observable.throw({name: "GuildNotFound"}),
+        )
+      )
+      .flatMap((server) =>
+        server.isUserAnAdmin(userId)
+          .flatMap((isAdmin) =>
+            Rx.Observable.if(
+              () => isAdmin,
+              Rx.Observable.of(server),
+              Rx.Observable.throw({name: "NotAuthorized"})
+            )
+          )
+      )
+      .subscribe(
+        (server) => {
+          res.locals.server = server;
+          next();
+        },
+        (error) => {
+          switch (error.name) {
+            case "NotAuthorized":
+            case "GuildNotFound":
+              return res.status(404).json({error: "Guild not found"});
+            default:
+              throw next(error);
+          }
+        }
+      )
   },
 };
